0244b0a1a9273711a08e614cac2685a646b7dbb7
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-14 15:14:47 -0500

    Everything working

--- my-awesome-quiz-app/src/app/results/results.component.ts
+++ my-awesome-quiz-app/src/app/results/results.component.ts
@@ -6,6 +6,7 @@ import { Answers } from '../quiz.model';
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
+
 export class ResultsComponent {
   @Input() answers: Answers;
 }
