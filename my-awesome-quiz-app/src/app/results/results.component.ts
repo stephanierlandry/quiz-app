03a70f8f01ec307d8c2e183abfd46b3d31bcbd48
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-14 13:12:18 -0500

    Results component modified to accept answers

--- my-awesome-quiz-app/src/app/results/results.component.ts
+++ my-awesome-quiz-app/src/app/results/results.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Answers } from '../quiz.model';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.sass']
 })
-export class ResultsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ResultsComponent {
+  @Input() answers: Answers;
 }
