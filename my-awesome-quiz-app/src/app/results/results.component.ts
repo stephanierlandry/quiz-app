b752745f2d49d466e94e909c22e7c70efc15daac
Stephanierlandry <stephanierlandry@gmail.com>
2020-07-16 10:04:24 -0500

    results component modified to return a message about results

--- my-awesome-quiz-app/src/app/results/results.component.ts
+++ my-awesome-quiz-app/src/app/results/results.component.ts
@@ -10,4 +10,19 @@ import { Question } from '../quiz.model';
 
 export class ResultsComponent {
   @Input() answers: Answers;
+
+  answersCheck(incorrectNum: number) {
+    if (incorrectNum === 0) {
+      return 'Wow WHAT A SUPERSTAR! You are truly a trivia master!';
+    }
+    if (incorrectNum <= 2) {
+      return 'Great job! Thats like a B- performance!';
+    }
+    if (incorrectNum <= 4) {
+      return 'Ehh.. Not too bad but also, Maybe try harder?';
+    }
+    if (incorrectNum >= 6)
+      return 'You should probably stick to your day job!!';
+  }
+
 }
